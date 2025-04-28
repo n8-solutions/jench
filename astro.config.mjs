143017d0d40f8a5da2fe89a959b4f7d434aa5e71
cristalcorp <cristal@cristalcorp.com>
2025-04-28 10:52:27 +0200

    first try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ton-user-github.github.io/ton-repo/', // Remplace ici
+  vite: {
+    server: {
+      fs: {
+        strict: true,
+      },
+    },
+  },
+  output: 'static'
+});
+
