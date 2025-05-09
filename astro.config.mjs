b515431197668bae53a63defb51b65f941b8f4d4
cristalcorp <cristal@cristalcorp.com>
2025-05-09 19:35:15 +0200

    menu + logo + flex or not flex

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://n8-solutions.github.io/jench/',
   base: '/jench/',
+  trailingSlash: 'always',
   vite: {
     server: {
       fs: {
