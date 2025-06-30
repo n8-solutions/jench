3840063a685c1f5f6a95498d3247f6c87dfbc992
cristalcorp <cristal@cristalcorp.com>
2025-06-30 15:55:43 +0200

    landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  prefetch: true,
   site: 'https://n8-solutions.github.io/jench/',
   base: '/jench/',
   trailingSlash: 'always',
