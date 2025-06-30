a91b30cb13a85c11bb864f2a8e4d22cc62b48f43
cristalcorp <cristal@cristalcorp.com>
2025-06-30 18:06:35 +0200

    Updates

--- chunks/astro_H8scwEXU.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_w3sW4gXL.mjs';
-import 'clsx';
--- pages/projects/_slug_.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { p as page } from '../../chunks/_slug__CdgOigOn.mjs';
-export { renderers } from '../../renderers.mjs';
